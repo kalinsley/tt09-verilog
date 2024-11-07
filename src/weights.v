cde324a3974a0348c7f2783a735ff90091010bc1
kalinsley <kai.linsley@gmail.com>
2024-11-07 08:48:58 +0000

    PLEASE

--- src/weights.v
+++ src/weights.v
@@ -7,7 +7,7 @@ module weights #(
 ) (
     input clk_i, 
     input rst_ni,
-    output reg [WIDTH_P-1:0] weights_o [0:NUM_SYNAPSES-1] 
+    output [WIDTH_P-1:0] weights_o [0:NUM_SYNAPSES-1] 
 );
     
     wire [7:0] random_weight;   
@@ -19,17 +19,21 @@ module weights #(
         .random_value(random_weight)
     );
 
+    reg [WIDTH_P-1:0] weights_o_internal [0:NUM_SYNAPSES-1];
     integer i;
-    always @(posedge clk_i) begin
+
+    always @(posedge clk_i or negedge rst_ni) begin
         if (!rst_ni) begin
             for (i = 0; i < NUM_SYNAPSES; i = i + 1) begin
-                weights_o[i] <= 0;
+                weights_o_internal[i] <= 0;
             end
         end else begin  
             for (i = 0; i < NUM_SYNAPSES; i = i + 1) begin
-                weights_o[i] <= random_weight % (1 << WIDTH_P);        // use $urandom for unsigned?
+                weights_o_internal[i] <= random_weight % (1 << WIDTH_P);
             end
         end
     end
+    
+    assign weights_o = weights_o_internal;
     
 endmodule
